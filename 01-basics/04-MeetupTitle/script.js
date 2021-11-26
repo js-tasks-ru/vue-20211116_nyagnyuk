b63b475963a0d14c02dd24679a6a2bbe771a35a9
Vynokur Natalya <nyagnyuk@ukr.net>
2021-11-26 12:52:15 +0200

    01-basics task

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,30 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const MeetupTitle = {
+  data() {
+    return{
+      radioCheck: '',
+      title: ''
+    }
+  },
+
+  watch: {
+    radioCheck: {
+      immediate: true,
+      handler() {
+        fetchMeetupById(this.radioCheck).then(response => this.title = response.title);
+        // this.resultTitle();
+      }
+    }
+  },
+
+  // methods: {
+  //   resultTitle: function() {
+  //     fetchMeetupById(this.radioCheck).then(response => this.title = response.title);
+  //   }
+  // }
+}
+
+const app = createApp(MeetupTitle);
+const vm = app.mount('#app');

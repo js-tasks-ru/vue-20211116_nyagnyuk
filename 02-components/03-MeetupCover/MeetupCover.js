b9fd59bdbc16ffbd6eb65a850202ee5fcd3fc91e
Vynokur Natalya <nyagnyuk@ukr.net>
2021-11-30 10:05:13 +0200

    fixed meetupCover component

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -7,13 +7,15 @@ export default defineComponent({
     title: {
       type: String,
     },
+
     image:  {
       type: String,
     },
   },
 
   template: `
-    <div class="meetup-cover" :style="image &&  {'--bg-url': \`url('https://course-vue.javascript.ru/api/images/2')\` }">
+<!--    <div class="meetup-cover" :style="image &&  {'&#45;&#45;bg-url': \`url('https://course-vue.javascript.ru/api/images/5')\` }">-->
+    <div class="meetup-cover" :style="image &&  {'--bg-url': \`url(\${image})\` }">
         <h1 class="meetup-cover__title">{{ title }}</h1>
     </div>`,
 });
